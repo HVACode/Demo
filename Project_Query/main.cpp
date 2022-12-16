1560bf75f68892e76c881fce9e1d1e3d225a9db1
HVACode <89624428+HVACode@users.noreply.github.com>
2022-12-16 22:30:15 +0800

    main 函数

--- Project_Query/main.cpp
+++ Project_Query/main.cpp
@@ -2,33 +2,34 @@
 /// @brief 查询 argv[1] 文件名 argv[2]单词 argv[3]（可能）符号 argv[4] 单词
 int main(int argc, char *argv[])
 {
+
 	std::ifstream ifs(argv[1]);
 	TextQuery text(ifs);
 
-	if (argc == 2)
+	if (argc == 3)
 	{
-		Query q = Query(argv[2]);
+		Query q = Query(std::string(argv[2]));
 		QueryResult res = q.eval(text);
 		q.rep();
 		print(std::cout, res);
 	}
-	else if (argc == 3)
+	else if (argc == 4)
 	{
-		Query q = ~Query(argv[2]);
+		Query q = ~Query(argv[3]);
 		QueryResult res = q.eval(text);
 		q.rep();
 		print(std::cout, res);
 	}
 	else
 	{
-		if (argv[3] == "&")
+		if (std::string(argv[3]) == "&")
 		{
 			Query binaryQ = Query(argv[2]) & Query(argv[4]);
 			QueryResult resx = binaryQ.eval(text);
 			binaryQ.rep();
 			print(std::cout, resx);
 		}
-		else if (argv[3] == "|")
+		else if (std::string(argv[3]) == "|")
 		{
 			Query binaryQ = Query(argv[2]) | Query(argv[4]);
 			QueryResult resx = binaryQ.eval(text);
