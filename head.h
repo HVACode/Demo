12b13668317f81c72cf8120358e89ae4d7436838
HVACode <89624428+HVACode@users.noreply.github.com>
2022-12-16 21:35:03 +0800

    first commit

---
+++ head.h
@@ -0,0 +1,16 @@
+#ifndef __HEAD_H__
+#define __HEAD_H__
+
+#include <iostream>
+#include <vector>
+#include <memory>
+#include <map>
+#include <set>
+#include <stdio.h>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+#include <iterator>
+
+#endif
